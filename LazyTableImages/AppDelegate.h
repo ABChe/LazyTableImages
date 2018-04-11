441cbe8e7dff7bab17a5bf4967012c4d5b264aa6
ABChe <abche777@163.com>
2018-04-11 20:33:31 +0800

    LazyTableImages
    start

---
+++ LazyTableImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LazyTableImages
+//
+//  Created by 车 on 2018/4/11.
+//  Copyright © 2018年 车. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
